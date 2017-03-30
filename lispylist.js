fb0f03dd8a585af93d19af796fe98c5d3331c018
Emil Hammarstedt <emil.hammarstedt@gmail.com>
2017-03-30 20:01:21 +0200

    cleanup and fixed sort re written as es 6

--- lispylist.js
+++ lispylist.js
@@ -4,114 +4,101 @@ var list = function (arr) {
     /*
      * a Lisp aproach to a list implementation in javascript
      */
-    var listBuilder = function (arr, i) {
-        if (i + 1 === arr.length)
-            return pair(arr[i], nil);
-        else
-            return pair(arr[i], listBuilder(arr, i + 1));
-    };
-    
+    var listBuilder = (arr, i) =>
+        (i + 1 === arr.length) ?
+            pair(arr[i], nil) :
+            pair(arr[i], listBuilder(arr, i + 1))
+
     return arr.length > 0 ? listBuilder(arr, 0) : nil;
 };
 
 var pair = function (a, b) {
-    var cons = function (a, b) {
-        var p = function (p) {
-            return p ? a : b;
-        };        
-        p.head = function () {
-            return this(true);
-        };
-        p.rest = function () {
-            return this(false);
-        };
-        p.equal = function (a) {
-            return this === a;
-        };
-        p.toString = function () {
-            return '( ' + p.head() + ' , ' + p.rest() + ' )';
-        };
-        p.len = function () {
-            if(nil.equal(p))
-                return 0;
-            else
-                return nil.equal(p.rest()) ? 1 : 1 + p.rest().len();
-        };
-        p.get = function (i) {
-            if(i <= 0){
-                return p.head();
-            }else if(nil.equal(p.rest())){
-                return nil;
-            }else
-                return p.rest().get(i - 1);
-        };
-        p.append = function(l) {
-            if(nil.equal(p))
-                return l;            
-            if(nil.equal(p.rest()))
-                return pair(p.head(),l);
-            else
-                return pair(p.head(),p.rest().append(l));                
-        }
-        p.map = function (fn) {
-            return p.merge(fn, pair);
-        };
-        p.fold = function (fn) {
-            return p.merge(function (e) {
-                return e;
-            }, fn);
-        }
-        p.forEach = function (fn) {
-            if (!nil.equal(p)) {
-                fn(p.head());
-                p.rest().forEach(fn);
-            }
-        };
-        p.merge = function (modifierFn, concatFn) {
-            if(nil.equal(p))
-                return nil;
-            else if (nil.equal(p.rest()))
-                return modifierFn(p.head());
-            else
-                return concatFn(modifierFn(p.head()), p.rest().merge(modifierFn, concatFn));
-        };
-        p.reverse = function () {            
-            if (nil.equal(p.rest()))
-                return p;
-            else
-                return p.rest().reverse().append( list([p.head()]) );
-        };
-        p.normalize = function(){
-            //todo
-        };
-        p.sort = function(cmp){ //quick-sort
-            var pivot = p.head();            
-            var left=nil,right=nil;
-            cmp = cmp === undefined ? function(a,b){return a < b;}: cmp; //defaults to numerical less then 
-            var partion = function(l){
-                if(cmp(l.head(), pivot))
-                    left = pair(l.head(),left);
-                else
-                    right = pair(l.head(),right);    
-                if(!nil.equal(l.rest())) 
-                    partion(l.rest()); 
-            };            
-            if(!nil.equal(p) && !nil.equal(p.rest())){
-                partion(p.rest());    
-                return left.sort().append(pair(pivot,nil)).append(right.sort());                        
-            }else{
-                return p;
-            }
-        };
-        return p;
-    };
-    return cons(a, b);
-};
+    var p = (p) => p ? a : b
 
-var nil = pair(null, null);
-nil.toString = function () {
-    return 'nil';
-};
+    p.head = function () { return this(true) }
+
+    p.rest = function () { return this(false) }
+
+    p.equal = function (a) { return this === a }
+
+    p.isNil = function () { return this === nil }
+
+    p.toString = () => '( ' + p.head() + ' . ' + p.rest() + ' )'
+
+    p.len = () => p.isNil() ? 0 : p.rest().isNil() ? 1 : 1 + p.rest().len()
+
+    p.get = (i) =>
+        (i <= 0) ?
+            p.head() :
+            (
+                (p.rest().isNil()) ?
+                    nil :
+                    p.rest().get(i - 1)
+            )
+
+    p.append = (l) =>
+        (p.isNil()) ?
+            l :
+            (
+                (p.rest().isNil()) ?
+                    pair(p.head(), l) :
+                    pair(p.head(), p.rest().append(l))
+            )
+
+    p.map = (fn) => p.merge(fn, pair)
+
+    p.fold = (fn) => p.merge((e) => e, fn)
+
+    p.forEach = (fn) => {
+        if (!p.isNil()) {
+            fn(p.head());
+            p.rest().forEach(fn);
+        }
+    };
+
+    p.merge = (modifierFn, concatFn) =>
+        (nil.equal(p)) ?
+            nil :
+            (
+                (nil.equal(p.rest())) ?
+                    modifierFn(p.head()) :
+                    concatFn(
+                        modifierFn(p.head()),
+                        p.rest().merge(modifierFn, concatFn))
+            )
+
+    p.reverse = () =>
+        (p.rest().isNil()) ?
+            p :
+            p.rest().reverse().append(list([p.head()]))
+
+    p.flatten = () => {
+        if (p.head())
+    }
+    p.sort = (cmp) => { //quick-sort
+        let pivot = p.head();
+        let left = nil, right = nil;
+        cmp = !cmp ? (a, b) => a < b : cmp //defaults to numerical less then 
+        let partion = (l) => {
+            if (cmp(l.head(), pivot))
+                left = pair(l.head(), left);
+            else
+                right = pair(l.head(), right);
+            if (!l.rest().isNil())
+                partion(l.rest())
+        }
+        if (!p.isNil() && !p.rest().isNil()) {
+            partion(p.rest())
+            return left.sort(cmp).append(pair(pivot, nil)).append(right.sort(cmp))
+        } else {
+            return p
+        }
+    }
+    return p
+}
+
+var nil = pair(null, null)
+nil.toString = () => 'nil'
 
 
 
